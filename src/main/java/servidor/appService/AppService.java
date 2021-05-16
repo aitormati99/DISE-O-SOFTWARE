5668999b1f6bc638765dcd9c9c677ec1712aa3fe
anebollo <anebollo@gmail.com>
2021-05-16 21:34:53 +0200

    api rest

--- src/main/java/servidor/appService/AppService.java
+++ src/main/java/servidor/appService/AppService.java
@@ -183,8 +183,9 @@ public class AppService{
 
         boolean extraido=false;
 
+
         try {
-            gateway= new Gateway("users/"+accesPoint+"/repos");
+            gateway= new Gateway("repos/"+accesPoint+"/contributors");
             Response res1 = gateway.makeGetRequest("");
 
             // Parse the response as JsonArray
@@ -232,13 +233,14 @@ public class AppService{
         return extraido;
     }
 
-    public boolean extraerContributorsUsers(String accesPoint, int a)throws RemoteException{
+    public boolean extraerCommitsUsers(String accesPoint, int a)throws RemoteException{
 
 
         boolean extraido=false;
 
+
         try {
-            gateway= new Gateway("users/"+accesPoint+"/repos");
+            gateway= new Gateway("repos/"+accesPoint+"/commits");
             Response res1 = gateway.makeGetRequest("");
 
             // Parse the response as JsonArray
@@ -251,15 +253,14 @@ public class AppService{
                 HashMap<String, String> object1 = (HashMap<String, String>) array.get(i);
                 //System.out.println(object1);
 
-                String usuario = object1.get("login");
-                listaLogin.add(usuario);
-            }
-            //como no hay nos lo inventamos
-            int id_equipo=+1;
+                String id_commits = object1.get("sha");
+                int id_commit_int=parseInt(id_commits);
+                listaCommit.get(i).setIdcommit(id_commit_int);
 
-            listaEquipos.get(a).setIdEquipo(id_equipo);
-            //esto creo que esta mal, nose cuando podemos saber esto
-            listaEquipos.get(a).setFecha_ini(listaProyectos.get(a).getProy_fecha_ini());
+                int e=i;
+                extraerMasInfoCommitsUsers(accesPoint,id_commits,e);
+
+            }
 
             for(int e=0; e<listaLogin.size();e++) {
                 for (int c = 0; c < listaUsuarios.size(); c++) {
@@ -281,10 +282,56 @@ public class AppService{
             System.out.println("Catched exception: " + e.getMessage());
         }
 
-        https://api.github.com/repos/mojombo/30daysoflaptops.github.io/contributors
 
         return extraido;
     }
+
+    public boolean extraerMasInfoCommitsUsers(String accesPoint, String id_commit,int a)throws RemoteException{
+
+
+        boolean extraido=false;
+        https://api.github.com/repos/mojombo/30daysoflaptops.github.io/commits/sha
+
+        try {
+            gateway= new Gateway("repos/"+accesPoint+"/commits/"+id_commit);
+            Response res1 = gateway.makeGetRequest("");
+
+            // Parse the response as JsonArray
+            JSONArray array = res1.readEntity(JSONArray.class);
+            //System.out.println(array.size());
+
+            ArrayList<String>listaLogin=new ArrayList<>();
+            //ESTA PARTE NOSE SI HAY QUE HACERLO EN EL METODO MAPEO DE DAO
+            for(int i=0; i<array.size();i++) {
+                HashMap<String, String> object1 = (HashMap<String, String>) array.get(i);
+                //System.out.println(object1);
+
+                String addition_commits = object1.get("stats.addition");
+                int addition_commits_int=parseInt(addition_commits);
+                String deletion_commits=object1.get("stats.deletion");
+                int deletion_commits_int=parseInt(deletion_commits);
+                listaCommit.get(a).setAdditionlines(addition_commits_int);
+                listaCommit.get(a).setDeletionlines(deletion_commits_int);
+
+
+            }
+
+
+
+
+            // since and per_page parameters are also relevant
+            // curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/users?per_page=2&since=5"
+
+        } catch (Exception e) {
+
+            System.out.println("Catched exception: " + e.getMessage());
+        }
+
+
+        return extraido;
+    }
+
+
     public int parseInt(String num){
         int numString = Integer.parseInt(num);
         return numString;
